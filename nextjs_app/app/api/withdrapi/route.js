b7b5c8710143763353400489f22c7847e263a389
SURENDRA-KUMAR-7 <117816234+SURENDRA-KUMAR-7@users.noreply.github.com>
2023-09-25 15:30:05 +0530

    Update route.js

--- nextjs_app/app/api/withdrapi/route.js
+++ nextjs_app/app/api/withdrapi/route.js
@@ -46,7 +46,8 @@ return NextResponse.json(result,{status:403});
 export async function POST(req) {
     let result;
     try{
-    const isUser = await dataFromToken(req);
+        const token = await req.cookies.get("authToken")?.value || '';
+    const isUser = await dataFromToken(token);
         if(isUser.success){
             const {paymentMode, payment} = await req.json();
             const userid = (isUser.userId).toString();
